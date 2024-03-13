52e4c7dcb418ddc32a452446d036cce1892d90d3
Jasiel-Z <zS21013907@estudiantes.uv.mx>
2024-03-12 21:54:34 -0600

    Primer borrador para GUI de registro de solicitud

--- GUI_RegisterRequest.xaml.cs
+++ GUI_RegisterRequest.xaml.cs
@@ -23,5 +23,15 @@ namespace SGMP_Client
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
